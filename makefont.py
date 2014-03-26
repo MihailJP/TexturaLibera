f41bc7fa5a96c538df508c114da5c9d64b931778
TANIGAWA Takashi <mihailjp@gmail.com>
2014-03-27 00:36:13 +0900

    Debug and adjust script

--- makefont.py
+++ makefont.py
@@ -12,7 +12,7 @@ CondensedFontFile = "TexturaLibera-Condensed.sfdir"
 
 penDegrees = (0, 45, 90, 135)
 
-WorkScale = 2.5
+WorkScales = [1.0, 1.25, 1.6, 2.0, 2.5, 3.2, 4.0, 6.4, 8.0]
 
 FamilyName = "TexturaLibera"
 HumanReadableFamilyName = "Textura Libera"
@@ -30,15 +30,15 @@ WeightDat = [
 ]
 
 WidthDat = [
-	{'Name': 'UC', 'HumanReadableName': 'Ultra-Condensed', 'Abbr': 'UCond'   , 'Panose': 2},
-	{'Name': 'XC', 'HumanReadableName': 'Extra-Condensed', 'Abbr': 'ExCond'  , 'Panose': 2},
-	{'Name': 'C' , 'HumanReadableName': 'Condensed'      , 'Abbr': 'Cond'    , 'Panose': 3},
-	{'Name': 'SC', 'HumanReadableName': 'Semi-Condensed' , 'Abbr': 'SemiCond', 'Panose': 3},
-	{'Name': ''  , 'HumanReadableName': ''               , 'Abbr': ''        , 'Panose': 4},
-	{'Name': 'SX', 'HumanReadableName': 'Semi-Expanded'  , 'Abbr': 'SemiEx'  , 'Panose': 5},
-	{'Name': 'X' , 'HumanReadableName': 'Expanded'       , 'Abbr': 'Expand'  , 'Panose': 5},
-	{'Name': 'XX', 'HumanReadableName': 'Extra-Expanded' , 'Abbr': 'ExEx'    , 'Panose': 6},
-	{'Name': 'UX', 'HumanReadableName': 'Ultra-Expanded' , 'Abbr': 'UEx'     , 'Panose': 6},
+	{'Name': 'UC', 'HumanReadableName': ' Ultra-Condensed', 'Abbr': ' UCond'   , 'Panose': 2},
+	{'Name': 'XC', 'HumanReadableName': ' Extra-Condensed', 'Abbr': ' ExCond'  , 'Panose': 2},
+	{'Name': 'C' , 'HumanReadableName': ' Condensed'      , 'Abbr': ' Cond'    , 'Panose': 3},
+	{'Name': 'SC', 'HumanReadableName': ' Semi-Condensed' , 'Abbr': ' SemiCond', 'Panose': 3},
+	{'Name': ''  , 'HumanReadableName': ''                , 'Abbr': ''         , 'Panose': 4},
+	{'Name': 'SX', 'HumanReadableName': ' Semi-Expanded'  , 'Abbr': ' SemiEx'  , 'Panose': 5},
+	{'Name': 'X' , 'HumanReadableName': ' Expanded'       , 'Abbr': ' Expand'  , 'Panose': 5},
+	{'Name': 'XX', 'HumanReadableName': ' Extra-Expanded' , 'Abbr': ' ExEx'    , 'Panose': 6},
+	{'Name': 'UX', 'HumanReadableName': ' Ultra-Expanded' , 'Abbr': ' UEx'     , 'Panose': 6},
 ]
 
 WeightCode = None #yet. This will be set later.
@@ -124,8 +124,8 @@ BaseFont.alterKerningClass(
 # Set output font properties
 BaseFont.strokedfont = False
 BaseFont.fontname = FamilyName + WidthDat[WidthCode]['Name'] + "-" + WeightDat[WeightCode]['Name']
-BaseFont.familyname = HumanReadableFamilyName + " " + WidthDat[WidthCode]['HumanReadableName']
-BaseFont.fullname = HumanReadableFamilyName + " " + WidthDat[WidthCode]['Abbr'] + " "+ WeightDat[WeightCode]['Abbr']
+BaseFont.familyname = HumanReadableFamilyName + WidthDat[WidthCode]['HumanReadableName']
+BaseFont.fullname = HumanReadableFamilyName + WidthDat[WidthCode]['Abbr'] + " " + WeightDat[WeightCode]['Abbr']
 BaseFont.weight = WeightDat[WeightCode]['HumanReadableName']
 BaseFont.os2_weight = (WeightCode + 1) * 100
 BaseFont.os2_width = (WidthCode + 1)
@@ -146,29 +146,45 @@ for glyph in Interpolated.glyphs():
 	if glyph.isWorthOutputting():
 		stdout.write("\r" + (" " * 40) + "\r" + glyph.glyphname)
 		stdout.flush()
+		glyph.round()
 		for layerID in range(1, 4):
 			if not glyph.layers[layerID].isEmpty():
-				layer = dupLayer(glyph.layers[layerID])
-				layer.transform(scale(WorkScale))
-				penScale = 0.8 if BaseFont[glyph.glyphname].color == 0x00ff00 else 1.0
-				layer.stroke(
-					"caligraphic",
-					float(argv[2]) / 5.0 * WorkScale * penScale,
-					float(argv[2]) * 0.06 * WorkScale * penScale,
-					rad(penDegrees[layerID]),
-					("removeinternal",) if BaseFont[glyph.glyphname].color == 0xffff00 else (None,))
-				layer.transform(scale(1.0 / WorkScale))
+				for WorkScale in WorkScales:
+					layer = dupLayer(glyph.layers[layerID])
+					layer.transform(scale(WorkScale))
+					penScale = 0.8 if BaseFont[glyph.glyphname].color == 0x00ff00 else 1.0
+					layer.stroke(
+						"caligraphic",
+						float(argv[2]) / 5.0 * WorkScale * penScale,
+						float(argv[2]) * 0.06 * WorkScale * penScale,
+						rad(penDegrees[layerID]),
+						("removeinternal",) if BaseFont[glyph.glyphname].color == 0xffff00 else (None,))
+					layer.transform(scale(1.0 / WorkScale))
+					allClosed = True
+					for contour in layer:
+						if not contour.closed:
+							allClosed = False
+					if allClosed: break
+				else:
+					raise RuntimeError("Failed to generate the stroke")
 				glyph.layers[layerID] = layer
 		for layerID in range(2, 4):
 			if not glyph.layers[layerID].isEmpty():
 				glyph.layers[1] += dupLayer(glyph.layers[layerID])
+		glyph.round()
 		glyph.removeOverlap()
 		glyph.round()
 		for layerID in range(1, 4):
 			BaseFont[glyph.glyphname].layers[layerID] = dupLayer(glyph.layers[1])
-		BaseFont[glyph.glyphname].autoHint()
 stdout.write("\r" + (" " * 40) + "\rDone\n")
 stdout.flush()
+
+# Hinting
+BaseFont.selection.none()
+for glyph in BaseFont.glyphs():
+	if glyph.isWorthOutputting():
+		BaseFont.selection.select(("more",), glyph.glyphname)
+BaseFont.autoHint()
 
 # Save font
 BaseFont.save(argv[1])
